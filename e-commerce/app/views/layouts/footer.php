6aac573164be31102ca36d1dad1eeb40711f030d
AlexisNguemby <alexis.nguemby-mbina@laplatefo.io>
2025-04-28 22:03:11 +0200

    archi

--- e-commerce/app/views/layouts/footer.php
+++ e-commerce/app/views/layouts/footer.php
@@ -4,8 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../../../public/assets/style/footer.css">
+
 </head>
 <body>
-    
+    <footer>
+        <div class="footer-content">
+            <p>&copy; <?php echo date("Y"); ?> E-Commerce Website. All rights reserved.</p>
+            <ul class="social-links">
+                <li><a href="#">Twitter</a></li>
+                <li><a href="#">Instagram</a></li>
+            </ul>
+        </div>
+    </footer>
 </body>
 </html>
+
